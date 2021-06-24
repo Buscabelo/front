6b35e7af2d2cc9987afa6839667a1f8df0c909e9
Joao Lucas Farias de Almeida <farias.almeida@academico.ifrn.edu.br>
2021-06-24 20:28:10 -0300

    Create Divider Component

---
+++ src/app/components/Divider/Divider.jsx
@@ -0,0 +1,37 @@
+export default function Divider({ size = 2, position = null, custom = null }) {
+  let style = {
+    border: 'none'
+  }
+
+  const capitalize = str => str && str[0].toUpperCase() + str.slice(1)
+  size = size + 'rem';
+
+  if (!custom) {
+    if (position == 'horizontal') {
+      style = {
+        ...style,
+        marginRight: size,
+        marginLeft: size
+      }
+    } else if (position == 'vertical') {
+      style = {
+        ...style,
+        marginTop: size,
+        marginBottom: size
+      }
+    } else if (!!position) {
+      style[`margin${capitalize(position)}`] = size;
+    } else {
+      style = {
+        ...style,
+        margin: size
+      }
+    }
+  } else {
+    style = custom;
+  }
+
+  return (
+    <hr style={style}/>
+  )
+}
--- src/app/pages/Home/Home.jsx
+++ src/app/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import AppLayout from '../../components/AppLayout/AppLayout';
+import Divider from '../../components/Divider/Divider';
 
 export default function Home() {
   return (
     <AppLayout>
       <>
+        <Divider size={1} />
       </>
     </AppLayout>
   )
