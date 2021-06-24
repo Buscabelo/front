712c3695ac923e0aca54b2ae775e96528280d214
Joao Lucas Farias de Almeida <farias.almeida@academico.ifrn.edu.br>
2021-06-24 20:17:22 -0300

    Create project structure

---
+++ src/app/App.jsx
@@ -0,0 +1,10 @@
+import AppLayout from './components/AppLayout/AppLayout'
+import Routes from './routes'
+
+export default function App() {
+  return (
+    <AppLayout>
+      <Routes />
+    </AppLayout>
+  );
+}
---
+++ src/app/components/AppLayout/AppLayout.jsx
@@ -0,0 +1,12 @@
+
+import './AppLayout.css';
+
+export default function AppLayout({ children }) {
+  return (
+    <>
+      <main>
+        {children}
+      </main>
+    </>
+  )
+}
