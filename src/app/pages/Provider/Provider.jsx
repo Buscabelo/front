6851f589bd603ea5600dec5d51ee6438b1154b46
Joao Lucas Farias de Almeida <farias.almeida@academico.ifrn.edu.br>
2021-07-06 22:17:33 -0300

    Integrate Provider page

--- src/app/pages/Provider/Provider.jsx
+++ src/app/pages/Provider/Provider.jsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { isMobile } from 'react-device-detect';
 import { MdGridOn, MdFormatListBulleted } from 'react-icons/md';
+import { useHistory, useParams } from 'react-router-dom';
 
 import './Provider.css';
 import AppLayout from '../../components/AppLayout/AppLayout';
@@ -9,7 +10,10 @@ import List from '../../components/List/List';
 import Service from '../../components/Service/Service';
 
 export default function Provider() {
+  const history = useHistory();
+  const { id } = useParams();
   const [direction, setDirection] = useState('horizontal');
+  const [data, setData] = useState(null);
 
   useEffect(() => {
     if (isMobile) {
@@ -17,9 +21,32 @@ export default function Provider() {
     }
   }, [])
 
+  const loadProvider = useCallback(() => {
+    fetch(`${process.env.REACT_APP_API}/providers/${id}`)
+      .then((response) => response.json())
+      .then(({data}) => {
+        setData(data);
+      })
+      .catch((error) => {
+        if (history.length > 1) {
+          history.goBack();
+        } else {
+          history.replace('/');
+        }
+      })
+  }, [id, setData])
+
+  useEffect(() => {
+    loadProvider();
+  }, [loadProvider])
+
   const handleDirectionChange = (changedDirection) => {
     if (direction !== changedDirection)
-      setDirection(changedDirection)
+      setDirection(changedDirection);
+  }
+
+  if (!data) {
+    return null;
   }
 
   return (
@@ -30,23 +57,19 @@ export default function Provider() {
       <Divider size={1} />
       <header className="provider-header">
         <aside>
-          <img src="https://picsum.photos/200/250" alt="Avatar Estabelecimento" />
+          {data.avatar && <img src={data.avatar} alt={`Imagem do estabelecimento ${data.name}`} />}
         </aside>
         <main>
-          <h2>Barbearia Dom Manuel</h2>
+          <h2>{data.name}</h2>
           <p className="info">
             <span className="opened">Aberto</span>
             •
             <span className="">Fecha às 17:00</span>
           </p>
-          {!isMobile && <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
-          </p>}
+          {!isMobile && <p>{data.description}</p>}
         </main>
       </header>
-      {isMobile && <p className="provider-description">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
-        </p>}
+      {isMobile && <p className="provider-description">{data.description}</p>}
       <Divider size={1} />
       <div className="services-header">
         <h3 className="services-title">Serviços</h3>
@@ -60,7 +83,7 @@ export default function Provider() {
         direction={direction}
         itemsPerLine={2}
         ItemComponent={Service}
-        items={[{id: 2, icon: 'https://picsum.photos/200/200', title: 'Corte Brabo', link: '', description: 'R$ 30,00', rating: '4,0'},{id: 5, icon: 'https://picsum.photos/200/200', title: 'Corte Brabo', link: '', description: 'R$ 25,00', rating: '4,0'},{id: 6, icon: 'https://picsum.photos/200/200', title: 'Corte Brabo', link: '', description: 'R$ 16,50', rating: '4,0'},{id: 7, icon: 'https://picsum.photos/200/200', title: 'Corte Brabo', link: '', description: 'R$ 65,50', rating: '4,0'}]}
+        items={[{id: 2, icon: 'https://picsum.photos/200/200', title: 'Corte 01', link: '', description: 'R$ 30,00', rating: '4,0'},{id: 5, icon: 'https://picsum.photos/200/200', title: 'Corte 02', link: '', description: 'R$ 25,00', rating: '4,0'},{id: 6, icon: 'https://picsum.photos/200/200', title: 'Corte 03', link: '', description: 'R$ 16,50', rating: '4,0'},{id: 7, icon: 'https://picsum.photos/200/200', title: 'Corte 04', link: '', description: 'R$ 65,50', rating: '4,0'}]}
       />
       <Divider size={1} />
     </AppLayout>
