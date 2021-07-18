9d4d2188b48c1c7ac0e8bc36a53f9c71128d3536
Joao Lucas Farias de Almeida <farias.almeida@academico.ifrn.edu.br>
2021-07-18 09:19:52 -0300

    Add Rate feature

--- src/app/pages/Appointment/Appointment.jsx
+++ src/app/pages/Appointment/Appointment.jsx
@@ -1,8 +1,12 @@
+import { useState } from 'react';
+
 import './Appointment.css';
 import AppLayout from '../../components/AppLayout/AppLayout';
 import Divider from '../../components/Divider/Divider';
 
 export default function Appointment() {
+  const [rate, setRate] = useState(null);
+
   return (
     <AppLayout>
       <Divider size={1} />
@@ -16,6 +20,19 @@ export default function Appointment() {
             <li>
               <h2>Corte Certo</h2>
               <p><b>Finalizado em:</b> 11/07/2021 - 15:40</p>
+              <p>
+                Avaliação: 
+                {!rate ? <select onChange={({target}) => setRate(target.value)}>
+                  <option value="1">1</option>
+                  <option value="1">2</option>
+                  <option value="1">3</option>
+                  <option value="1">4</option>
+                  <option value="1">5</option>
+                </select>
+                  :
+                  rate
+                }
+              </p>
               <p>Valor: R$ 25,00</p>
             </li>
           </ol>
