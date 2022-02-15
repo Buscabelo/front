387b3b3c38463841d051c692872489a16c13cb30
jlucfarias <jlalmeida.farias@gmail.com>
2022-02-15 19:49:21 -0300

    Reorganizando páginas refatoradas

--- src/ui/modules/customer/pages/Profile/index.jsx
+++ src/ui/modules/customer/pages/Profile/index.jsx
@@ -1,36 +1,45 @@
 import { isMobile, isTablet } from 'react-device-detect';
-import { MdEdit, MdList, MdLogout } from 'react-icons/md';
+import { MdEdit, MdCalendarToday, MdLogout } from 'react-icons/md';
 import { useHistory } from 'react-router-dom';
 
 import './styles.css';
+import Layout from '../../../common/components/CustomerLayout';
 
 export default function Profile() {
   const history = useHistory();
   const user = JSON.parse(localStorage.getItem('@buscabelo_client/user'));
 
+  const logout = () => {
+    localStorage.removeItem('@buscabelo_client/user');
+    localStorage.removeItem('@buscabelo_client/token');
+    history.replace('/');
+  };
+
   if (isMobile || isTablet) {
     return (
-      <article className="profile-wrapper">
-        <header>
-          <img src={user.avatar || 'https://picsum.photos/300/300'} />
-        </header>
-        <main>
-          <nav>
-            <a href="#">
-              <MdEdit />
-              Editar Perfil
-            </a>
-            <a href="/agendamentos">
-              <MdList />
-              Meus Agendamentos
-            </a>
-            <a href="/agendamentos">
-              <MdLogout />
-              Sair
-            </a>
-          </nav>
-        </main>
-      </article>
+      <Layout>
+        <article className="profile-wrapper">
+          <header>
+            <img src={user.avatar || 'https://picsum.photos/300/300'} />
+          </header>
+          <main>
+            <nav>
+              <a href="#">
+                <MdEdit />
+                Editar Perfil
+              </a>
+              <a href="/agendamentos">
+                <MdCalendarToday />
+                Meus Agendamentos
+              </a>
+              <a onClick={() => logout()}>
+                <MdLogout />
+                Sair
+              </a>
+            </nav>
+          </main>
+        </article>
+      </Layout>
     );
   }
 
