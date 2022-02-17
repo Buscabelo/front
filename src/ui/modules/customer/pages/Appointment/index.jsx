abb1657b041890ae96c28d67570a891578e86484
jlucfarias <jlalmeida.farias@gmail.com>
2022-02-16 21:18:00 -0300

    Refatorando página de detalhe do agendamento [mobile]

--- src/ui/modules/customer/pages/Appointment/index.jsx
+++ src/ui/modules/customer/pages/Appointment/index.jsx
@@ -1,11 +1,16 @@
 import { useCallback, useEffect, useState } from 'react';
 import { isMobile, isTablet } from 'react-device-detect';
 import { useHistory, useParams } from 'react-router-dom';
+import { MdCheckCircle, MdChevronLeft, MdClose, MdStar } from 'react-icons/md';
+import { format, parseISO } from 'date-fns';
 
 import './styles.css';
-import AppLayout from '../../components/AppLayout/AppLayout';
+import Layout from '../../../common/components/CustomerLayout';
 import Divider from '../../components/Divider/Divider';
 import { minStackLength } from '../../../../constants/history';
+
+const starsLength = 5;
+const decimalPlaces = 2;
 
 export default function Appointment() {
   const history = useHistory();
@@ -118,11 +123,134 @@ export default function Appointment() {
   };
 
   if (isMobile || isTablet) {
-    return null;
+    const renderStatus = () => {
+      if (data.time_done_at)
+        return <>concluído às {format(parseISO(data.time_done_at), 'H:mm - dd/MM/y')}</>;
+
+      if (data.canceled_at)
+        return <>cancelado às {format(parseISO(data.canceled_at), 'H:mm - dd/MM/y')}</>;
+
+      return null;
+    };
+
+    const renderIcon = () => {
+      if (data.time_done_at)
+        return <MdCheckCircle color="green" />;
+
+      if (data.canceled_at)
+        return <MdClose color="red" />;
+
+      return null;
+    };
+
+    const setRate = async rate => {
+      try {
+        const body = {
+          appointment: data.id,
+          customer: user.id,
+          rating_number: rate
+        };
+        const response = await fetch(`${process.env.REACT_APP_API}/appointments/${data.id}/rate`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+          },
+          body: JSON.stringify(body)
+        });
+        const { success, rating } = await response.json();
+
+        if (success) {
+          const _data = data;
+          _data.rate = +rating.rating_number;
+          setData(_data);
+        }
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.error(error);
+      }
+    };
+
+    const renderRate = () => {
+      if (data.rate)
+        return <>
+          <h3>Sua avaliação ao pedido</h3>
+          <section className="stars">
+            {[...Array(starsLength)].map((_, index) => (
+              <span
+                key={++index}
+                className={++index <= data.rate ? 'on' : ''}
+              >
+                <MdStar />
+              </span>
+            ))}
+          </section>
+        </>;
+
+      return <>
+        <h3>Avalie seu pedido</h3>
+        <section className="stars">
+          {[...Array(starsLength)].map((_, index) => (
+            <span
+              key={++index}
+              onClick={() => setRate(++index)}
+            >
+              <MdStar />
+            </span>
+          ))}
+        </section>
+        <p>Escolha de 1 a 5 estrelas para classificar</p>
+      </>;
+    };
+
+    return <Layout>
+      <article className="appointment-wrapper">
+        <nav>
+          <a onClick={() => history.length >= minStackLength ? history.goBack() : history.replace('/')}>
+            <MdChevronLeft />
+          </a>
+          <h1>Detalhes do Pedido</h1>
+        </nav>
+        <header>
+          <section className="provider">
+            <img src={data.provider.avatar || 'https://picsum.photos/30/30'} />
+            <b>{data.provider.name}</b>
+          </section>
+          <section className="appointment">
+            Realizada às {format(parseISO(data.scheduled_at), 'H:mm - dd/MM/y')}
+            <a href="#">Ver serviços</a>
+          </section>
+          <section className="status">
+            {renderIcon()} Serviço {renderStatus()}
+          </section>
+        </header>
+        <main>
+          <section className="appointment">
+            <ul>
+              <li>
+                <strong>{data.service.name}</strong>
+                <span>
+                  {format(parseISO(data.appointment_to), 'dd/MM/y')}
+                </span>
+              </li>
+            </ul>
+          </section>
+          <section className="payment">
+            <h3>Total</h3>
+            <b>R$ {data.service.value.toFixed(decimalPlaces).replace('.', ',')}</b>
+          </section>
+        </main>
+        {data.canceled_at && <footer>
+          <section className="rate">
+            {renderRate()}
+          </section>
+        </footer>}
+      </article>
+    </Layout>;
   }
 
   return (
-    <AppLayout>
+    <Layout>
       <Divider size={1} />
       <article className="item-container">
         <header>
@@ -141,6 +269,6 @@ export default function Appointment() {
         </main>
       </article>
       <Divider size={1} />
-    </AppLayout>
+    </Layout>
   );
 }
