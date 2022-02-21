76d4e8c4ebb50284b6f7be9a14781e1601887155
Jose Victor <zevicctor2@gmail.com>
2022-02-21 11:12:41 -0300

    Adicionando modal de detalhes do pedido

--- src/ui/modules/customer/pages/AppointmentDetail/index.jsx
+++ src/ui/modules/customer/pages/AppointmentDetail/index.jsx
@@ -6,7 +6,6 @@ import { format, parseISO } from 'date-fns';
 
 import './styles.css';
 import Layout from '../../../common/components/CustomerLayout';
-import Divider from '../../components/Divider/Divider';
 import { minStackLength } from '../../../../constants/history';
 
 const starsLength = 5;
@@ -24,7 +23,7 @@ export default function AppointmentDetail() {
       headers: {
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${token}`
-      },
+      }
     })
       .then(response => response.json())
       .then(({ success, appointment }) => {
@@ -45,82 +44,84 @@ export default function AppointmentDetail() {
     loadAppointment();
   }, [loadAppointment]);
 
-  const rateAppointment = event => {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    const body = Object.fromEntries(formData.entries());
-    body.appointment = data.id;
-    body.customer = user.id;
+  // const rateAppointment = event => {
+  //   event.preventDefault();
+  //   const formData = new FormData(event.target);
+  //   const body = Object.fromEntries(formData.entries());
+  //   body.appointment = data.id;
+  //   body.customer = user.id;
 
-    if (body.rating_number) {
-      fetch(`${process.env.REACT_APP_API}/appointments/${data.id}/rate`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`
-        },
-	      body: JSON.stringify(body)
-      })
-        .then(response => response.json())
-        .then(({ success, rating }) => {
-          if (success) {
-            const _data = data;
-            _data.rate = +rating.rating_number;
-            setData(_data);
-          }
-        })
-        .catch(error => {
-          // eslint-disable-next-line no-console
-          console.error(error);
-        });
-    }
-  };
+  //   if (body.rating_number) {
+  //     fetch(`${process.env.REACT_APP_API}/appointments/${data.id}/rate`, {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //         'Authorization': `Bearer ${token}`
+  //       },
+  //       body: JSON.stringify(body)
+  //     })
+  //       .then(response => response.json())
+  //       .then(({ success, rating }) => {
+  //         if (success) {
+  //           const _data = data;
+  //           _data.rate = +rating.rating_number;
+  //           setData(_data);
+  //         }
+  //       })
+  //       .catch(error => {
+  //         // eslint-disable-next-line no-console
+  //         console.error(error);
+  //       });
+  //   }
+  // };
 
   if (!data)
     return null;
 
-  const renderStatus = () => {
-    if (data.time_done_at)
-      return <span className="done">Finalizado</span>;
+  // essa parte comentada é pra vc (joao) ver se vai usar alguma coisa que esta aqui, se não for usar pode apagar!!!
 
-    if (data.canceled_at)
-      return <span className="canceled">Cancelado</span>;
+  // const renderStatus = () => {
+  //   if (data.time_done_at)
+  //     return <span className="done">Finalizado</span>;
 
-    return null;
-  };
+  //   if (data.canceled_at)
+  //     return <span className="canceled">Cancelado</span>;
 
-  const renderDate = () => {
-    if (data.time_done_at)
-      return <p><b>Finalizado em: </b> {data.time_done_at}</p>;
+  //   return null;
+  // };
 
-    if (data.canceled_at)
-      return <p><b>Cancelado em: </b> {data.canceled_at}</p>;
+  // const renderDate = () => {
+  //   if (data.time_done_at)
+  //     return <p><b>Finalizado em: </b> {data.time_done_at}</p>;
 
-    return null;
-  };
+  //   if (data.canceled_at)
+  //     return <p><b>Cancelado em: </b> {data.canceled_at}</p>;
 
-  const renderRate = () => {
-    if (data.rate)
-      return data.rate;
+  //   return null;
+  // };
 
-    return (
-      <>
-        <p style={{marginBottom: '15px'}}>Avaliação:</p>
-        <form style={{display: 'flex', flexDirection: 'column', alignItems: 'flex-start'}} onSubmit={event => rateAppointment(event)}>
-          <select style={{marginBottom: '15px'}} name="rating_number">
-            <option value="">Avalie o seu agendamento</option>
-            <option value="5">Excelente</option>
-            <option value="4">Bom</option>
-            <option value="3">Pode Melhorar</option>
-            <option value="2">Ruim</option>
-            <option value="1">Péssimo</option>
-          </select>
-          <textarea name="description" style={{padding: '1vh 0.5vw', marginBottom: '15px', resize: 'none'}} rows="8" cols="35" />
-          <button type="submit">Enviar</button>
-        </form>
-      </>
-    );
-  };
+  // const renderRate = () => {
+  //   if (data.rate)
+  //     return data.rate;
+
+  //   return (
+  //     <>
+  //       <p style={{marginBottom: '15px'}}>Avaliação:</p>
+  //       <form style={{display: 'flex', flexDirection: 'column', alignItems: 'flex-start'}} onSubmit={event => rateAppointment(event)}>
+  //         <select style={{marginBottom: '15px'}} name="rating_number">
+  //           <option value="">Avalie o seu agendamento</option>
+  //           <option value="5">Excelente</option>
+  //           <option value="4">Bom</option>
+  //           <option value="3">Pode Melhorar</option>
+  //           <option value="2">Ruim</option>
+  //           <option value="1">Péssimo</option>
+  //         </select>
+  //         <textarea name="description" style={{padding: '1vh 0.5vw', marginBottom: '15px', resize: 'none'}} rows="8" cols="35" />
+  //         <button type="submit">Enviar</button>
+  //       </form>
+  //     </>
+  //   );
+  // };
 
   if (isMobile || isTablet) {
     const renderStatus = () => {
@@ -250,25 +251,6 @@ export default function AppointmentDetail() {
   }
 
   return (
-    <Layout>
-      <Divider size={1} />
-      <article className="item-container">
-        <header>
-          <h1>{data.provider.name} {renderStatus()}</h1>
-          <p>Data do Agendamento: {data.appointment_to}</p>
-        </header>
-        <main>
-          <ol>
-            <li>
-              <h2>{data.service.name}</h2>
-              {renderDate()}
-              {data.time_done_at && renderRate()}
-              <p>Valor: R$ {data.service.value}</p>
-            </li>
-          </ol>
-        </main>
-      </article>
-      <Divider size={1} />
-    </Layout>
+    <></>
   );
 }
