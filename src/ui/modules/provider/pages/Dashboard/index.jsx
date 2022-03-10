d3d5f5430b7809e3ead16a57cbc15ddeb650c316
Jose Victor <zevicctor2@gmail.com>
2022-03-09 21:52:25 -0300

    Adicionando cards na dashboard do estabelecimento

--- src/ui/modules/provider/pages/Dashboard/index.jsx
+++ src/ui/modules/provider/pages/Dashboard/index.jsx
@@ -5,7 +5,24 @@ export default function Dashboard() {
 
   return (
     <DashboardLayout>
-      <p>iusbdiabsdhibaklshdbaklsdb</p>
+      <h1>Dashboard</h1>
+
+      <div className='container-list'>
+        <div className='card'>
+          <span className='title-card'>Rendimento (mensal)</span>
+          <p className='description-card'>R$ 500</p>
+        </div>
+
+        <div className='card'>
+          <span className='title-card'>Rendimento (Anual)</span>
+          <p className='description-card'>R$ 11500</p>
+        </div>
+
+        <div className='card'>
+          <span className='title-card'>Agendamentos realizados do mês</span>
+          <p className='description-card'>50</p>
+        </div>
+      </div>
     </DashboardLayout>
   );
 }
