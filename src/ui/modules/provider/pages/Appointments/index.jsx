935d42b6a7bd431fdbe4bf2f0dc72f45f2c5e7af
jlucfarias <jlalmeida.farias@gmail.com>
2022-03-11 19:20:19 -0300

    Refatorando página de agendamentos

--- src/ui/modules/provider/pages/Appointments/index.jsx
+++ src/ui/modules/provider/pages/Appointments/index.jsx
@@ -1,139 +1,114 @@
-import { useEffect, useState, useContext } from 'react';
-import { isMobile, isTablet } from 'react-device-detect';
-import { Card, Typography, Modal } from 'antd';
+import { useCallback, useContext, useEffect, useState } from 'react';
+import { format, parseISO, formatISO } from 'date-fns';
 
-import FlexlistWrapper from '../../components/FlexlistWrapper/FlexlistWrapper';
-import { ResponseHandlerContext } from '../../context/ResponseHandlerContext';
 import DashboardLayout from '../../components/DashboardLayout/DashboardLayout';
+import { AppContext } from '../../../common/context/AppContext';
+import './styles.css';
 
-const { Title, Text } = Typography;
-
-export default function ListAppointments() {
-  const [isModalVisible, setIsModalVisible] = useState(false);
+export default function Appointments() {
+  const { token } = useContext(AppContext);
   const [appointments, setAppointments] = useState([]);
-  const [selectedAppointment, setSelectedAppointment] = useState(null);
-  const [forceUpdate, setForceUpdate] = useState(true);
 
-  const { query, responseMessage } = useContext(ResponseHandlerContext);
+  const loadAppointments = useCallback(async () => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API}/appointments`, {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      });
+      const { success, appointments } = await response.json();
+
+      if (success) {
+        setAppointments(appointments);
+      }
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(error);
+    }
+  }, [token]);
 
   useEffect(() => {
-    (async() => {
-      const { response, statusCode } = await query({
-        link: `${process.env.REACT_APP_BACKEND_AD}/appointments`,
-        method: 'GET',
-        auth: true,
-      });
+    if (token) {
+      loadAppointments();
+    }
+  }, [loadAppointments, token]);
 
-      if (response) {
-        responseMessage({statusCode});
-        setAppointments(response?.appointments || []);
+  async function handleFinish(appointment_id) {
+    try {
+      const body = JSON.stringify({
+        time_done_at: formatISO(new Date())
+      });
+      const response = await fetch(`${process.env.REACT_APP_API}/appointments/finish/${appointment_id}`, {
+        method: 'PATCH',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
+        body
+      });
+      const { success } = await response.json();
+
+      if (success) {
+        window.location.reload();
       }
-    })();
-  }, [query, responseMessage, forceUpdate]);
-
-  async function handleSelectAppointment({id}) {
-    const { response } = await query({
-      link: `${process.env.REACT_APP_BACKEND_AD}/appointments/${id}`,
-      method: 'GET',
-      auth: true,
-    });
-
-    setSelectedAppointment(response?.appointment || []);
-    setIsModalVisible(true);
-  }
-
-  async function handleFinishService({id, target}) {
-    if (!target) {
-      setIsModalVisible(false);
-    } else if (target === 'Finalizar') {
-      const { response, statusCode } = await query({
-        link: `${process.env.REACT_APP_BACKEND_AD}/appointments/finish/${id}`,
-        method: 'PATCH',
-        auth: true,
-        body: JSON.stringify({ time_done_at: new Date().toISOString() })
-      });
-
-      setForceUpdate(!forceUpdate);
-      responseMessage({ statusCode, response });
-      setIsModalVisible(false);
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(error);
     }
   }
 
-  async function handleCancelService({id, target}) {
-    if (!target) {
-      setIsModalVisible(false);
-    } else if (target === 'Cancelar') {
-      const { statusCode } = await query({
-        link: `${process.env.REACT_APP_BACKEND_AD}/appointments/cancel/${id}`,
-        method: 'PATCH',
-        auth: true,
-        body: JSON.stringify({ canceled_at: new Date().toISOString() })
+  async function handleCancel(appointment_id) {
+    try {
+      const body = JSON.stringify({
+        canceled_at: formatISO(new Date())
       });
+      const response = await fetch(`${process.env.REACT_APP_API}/appointments/cancel/${appointment_id}`, {
+        method: 'PATCH',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
+        body
+      });
+      const { success } = await response.json();
 
-      setForceUpdate(!forceUpdate);
-      responseMessage({ statusCode });
-      setIsModalVisible(false);
+      if (success) {
+        window.location.reload();
+      }
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(error);
     }
-  }
-
-  if (isMobile || isTablet) {
-    return null;
   }
 
   return (
     <DashboardLayout>
-      {
-        selectedAppointment ?
-          <Modal
-            cancelButtonProps={{danger: true, disabled: selectedAppointment.canceled_at || selectedAppointment.time_done_at ? true : false }}
-            okButtonProps={{ disabled: selectedAppointment.canceled_at || selectedAppointment.time_done_at ? true : false }}
-            title="Agendamento"
-            visible={isModalVisible}
-            okText={'Finalizar'}
-            cancelText={'Cancelar'}
-            onOk={e => handleFinishService({id: selectedAppointment.id, target: e.target.innerText})}
-            onCancel={e => handleCancelService({id: selectedAppointment.id, target: e.target.innerText})}>
-            <p>{selectedAppointment.customer.name}</p>
-            <p>R$ {selectedAppointment.service.value}</p>
-            Agendado para <Text strong>{(() => {
-              const d = new Date(selectedAppointment.appointment_to);
-              const [data,] = d.toLocaleString().split(' ');
-              return data;
-            })()}</Text><br />
-            <Text>{selectedAppointment.service.name}</Text>
-          </Modal>
-          :
-          null
-      }
-
-      <FlexlistWrapper>
-        {appointments.map(({ appointment_to, id, service, customer, canceled_at, time_done_at }) => {
-          const d = new Date(appointment_to);
-          const [data,] = d.toLocaleString().split(' ');
-          return (
-            <div style={{
-              width: '300px'
-            }} key={id}>
-              <Card
-                onClick={() => handleSelectAppointment({ id })}
-                hoverable
-                title={<Title level={3}>{customer.name}</Title>}
-                bordered={false}
-                style={{
-                  width: 300,
-                  backgroundColor: canceled_at ? '#ff4d4f' : time_done_at ? '#bae637' : '#d6e4ff'
-                }}
-              >
-                <div style={{ display: 'flex', justifyContent: 'space-between' }} >
-                  <Text>R$ {service.value}</Text>
-                </div>
-                <Title level={5}>{data}</Title>
-                <Text>{service.name}</Text>
-              </Card>
-            </div>
-          );
-        })}
-      </FlexlistWrapper>
+      <table className="appointments-list">
+        <thead>
+          <tr>
+            <th>Usuário</th>
+            <th>Serviço</th>
+            <th>Agendado em</th>
+            <th>Agendado para</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          {appointments.map(appointment => <tr key={appointment.id}>
+            <td>
+              <img src={appointment.customer.avatar || 'https://picsum.photos/50/50'} />
+              {appointment.customer.name}
+            </td>
+            <td>{appointment.service.name}</td>
+            <td>{format(parseISO(appointment.scheduled_at), 'dd/MM/y H:mm')}</td>
+            <td>{format(parseISO(appointment.appointment_to), 'dd/MM/y H:mm')}</td>
+            <td>
+              {!appointment.canceled_at && <button type="button" className="danger" onClick={() => handleCancel(appointment.id)}>Cancelar</button>}
+              {!appointment.canceled_at && !appointment.time_done_at && <button type="button" className="success" onClick={() => handleFinish(appointment.id)}>Finalizar</button>}
+            </td>
+          </tr>)}
+        </tbody>
+      </table>
     </DashboardLayout>
   );
 }
