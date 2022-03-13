5b04dace329fa752644fddff826da93e088457f1
Jose Victor <zevicctor2@gmail.com>
2022-03-13 20:20:59 -0300

    adicionando form para edição de perfil

--- src/ui/modules/provider/router.jsx
+++ src/ui/modules/provider/router.jsx
@@ -3,9 +3,11 @@ import { Route } from 'react-router-dom';
 import Appointments from './pages/Appointments';
 import Dashboard from './pages/Dashboard';
 import Services from './pages/Services';
+import ProfileEdit from './pages/ProfileEdit';
 
 export default [
   <Route key="painel:home" exact path="/painel" component={Dashboard} />,
   <Route key="painel:servicos" exact path="/painel/servicos" component={Services} />,
   <Route key="painel:agendamentos" exact path="/painel/agendamentos" component={Appointments} />,
+  <Route key="painel:edicao" exact path="/painel/perfil/edicao" component={ProfileEdit} />,
 ];
